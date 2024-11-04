1f8a9f1fcb915d57d2b3d9dab9c230f8357fe450
Chenar Rzgar <aipassion2026@gmail.com>
2024-11-04 13:17:55 +0800

    Update request_6.sql

--- Sakila_database_queries/request_6.sql
+++ Sakila_database_queries/request_6.sql
@@ -1,7 +1,10 @@
 /*Monthly Rentals: Generate a summary of rentals by month for the current year, showing the total rentals per month.*/
 
-SELECT COUNT(rental_id) AS rentals, MONTH(rental_date) AS months FROM rental
+SELECT 
+  COUNT(rental_id) AS rentals, 
+  MONTH(rental_date) AS months 
+FROM 
+  rental
 GROUP BY MONTH(rental_date)
 ORDER BY MONTH(rental_date) ASC;
 
-SELECT * FROM rental;
