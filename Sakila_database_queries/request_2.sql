10751435f03dd6e7ec525b56871c46822675b055
Chenar Rzgar <aipassion2026@gmail.com>
2024-11-04 13:11:44 +0800

    Update request_2.sql

--- Sakila_database_queries/request_2.sql
+++ Sakila_database_queries/request_2.sql
@@ -1,9 +1,13 @@
 /*Film Inventory: List all films along with their rental rate and the number of copies available in the inventory.*/
 
-SELECT f.title as film_title, f.rental_rate, COUNT(i.film_id) AS num_copies
-FROM film f
-JOIN inventory i 
-ON f.film_id = i.film_id
+SELECT 
+  f.title as film_title,
+  f.rental_rate, 
+  COUNT(i.film_id) AS num_copies
+FROM 
+  film f
+JOIN 
+  inventory i ON f.film_id = i.film_id
 GROUP BY f.title, f.rental_rate
 ORDER BY f.title;
 
