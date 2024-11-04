3c5d316e475741973c3c1475ab203a4e3968ae64
Chenar Rzgar <aipassion2026@gmail.com>
2024-11-04 13:23:06 +0800

    Update request_8.sql

--- Sakila_database_queries/request_8.sql
+++ Sakila_database_queries/request_8.sql
@@ -1,8 +1,20 @@
-/*Customer Registration: List all customers who registered in the last year, including their registration date and any rentals they have made.*/ 
+/*Customer Registration: List all customers who registered in the last year,
+including their registration date and any rentals they have made.*/ 
 
-SELECT c.first_name, c.last_name, c.create_date AS registration_date, COUNT(r.rental_id) AS rentals
-FROM customer c JOIN rental r ON c.customer_id = r.customer_id
-WHERE YEAR(c.create_date) = 2006
-GROUP BY c.first_name, c.last_name, c.create_date
+SELECT 
+  c.first_name, 
+  c.last_name, 
+  c.create_date AS registration_date, 
+  COUNT(r.rental_id) AS rentals
+FROM 
+  customer c 
+JOIN 
+  rental r ON c.customer_id = r.customer_id
+WHERE 
+  YEAR(c.create_date) = 2006
+GROUP BY 
+  c.first_name,
+  c.last_name, 
+  c.create_date
 ORDER BY c.first_name, c.last_name ASC;
 
