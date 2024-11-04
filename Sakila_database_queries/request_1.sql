8af92c993fb1a628e1766e21de578884a994b5b8
Chenar Rzgar <aipassion2026@gmail.com>
2024-11-04 13:14:15 +0800

    Update request_1.sql

--- Sakila_database_queries/request_1.sql
+++ Sakila_database_queries/request_1.sql
@@ -1,12 +1,23 @@
 /*Customer Details: Retrieve the first name, last name, and email of all customers who have rented movies in the last 30 days.*/
 
-SELECT C.first_name, C.last_name, C.email, R.rental_date AS date FROM customer C JOIN rental R 
-ON C.customer_id = R.customer_id WHERE R.rental_date = "2006-02-14 15:16:03"
-GROUP BY C.first_name, C.last_name, C.email, R.rental_date
+SELECT 
+  C.first_name,
+  C.last_name, 
+  C.email, 
+  R.rental_date AS date 
+FROM 
+  customer C 
+JOIN 
+  rental R ON C.customer_id = R.customer_id
+WHERE R.rental_date = "2006-02-14 15:16:03"
+GROUP BY 
+  C.first_name, 
+  C.last_name, 
+  C.email, 
+  R.rental_date
 ORDER BY R.rental_date DESC;
 
-SELECT rental_date FROM rental
-ORDER BY rental_date DESC;
+
 
 
 
